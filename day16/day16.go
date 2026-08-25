2cd5eef6235b8fd89404ffa0245a6c898fac3482
Aadam Southon <a.southon@hotmail.com>
2026-08-25 22:50:20 +0000

    Added day 16 solutions

--- day16/day16.go
+++ day16/day16.go
@@ -1,10 +1,10 @@
 package day16
 
 import (
-	"bufio"
 	"errors"
 	"log"
 	"os"
+	"strings"
 )
 
 func Solve(part int, logger *log.Logger, inputFile ...string) (any, error) {
@@ -23,42 +23,133 @@ func Solve(part int, logger *log.Logger, inputFile ...string) (any, error) {
 	}
 }
 
-func SolvePart1(inputFile string, logger *log.Logger) int {
-	f, err := os.Open(inputFile)
+// Call the data you have at this point "a".
+// Make a copy of "a"; call this copy "b".
+// Reverse the order of the characters in "b".
+// In "b", replace all instances of 0 with 1 and all 1s with 0.
+// The resulting data is "a", then a single 0, then "b".
+func generateString(initialState string, length int) string {
+	str := initialState
+	for len(str) < length {
+		a := str
+		str = a + "0"
+		for i := len(a) - 1; i >= 0; i-- {
+			char := a[i]
+			switch char {
+			case '1':
+				str += "0"
+			case '0':
+				str += "1"
+			}
+		}
+	}
+	return str[:length]
+}
+
+// Consider each pair: 11, 00, 10, 11, 01, 00.
+// These are same, same, different, same, different, same, producing 110101.
+// The resulting string has length 6, which is even, so we repeat the process.
+// The pairs are 11 (same), 01 (different), 01 (different).
+// This produces the checksum 100, which has an odd length, so we stop.
+func checksum(str string) string {
+	checksum := str
+	for len(checksum)%2 == 0 {
+		prev := checksum
+		new := ""
+		for i := 0; i < len(prev)-1; i += 2 {
+			if prev[i] == prev[i+1] {
+				new += "1"
+			} else {
+				new += "0"
+			}
+		}
+		checksum = new
+	}
+	return checksum
+}
+
+func SolvePart1(inputFile string, logger *log.Logger) string {
+	f, err := os.ReadFile(inputFile)
 	if err != nil {
-		logger.Fatalf("error opening file: %v\n", err)
+		logger.Fatalf("error reading file: %v\n", err)
 	}
-	defer f.Close()
 
-	answer := 0
-
-	scanner := bufio.NewScanner(f)
-	i := -1
-	for scanner.Scan() {
-		i++
-		line := scanner.Text()
-
-		logger.Printf("%d : %s", i, line)
+	var diskLength int
+	if strings.HasPrefix(inputFile, "example") {
+		diskLength = 20
+	} else {
+		diskLength = 272
 	}
+
+	logger.Printf("Generating string of length %d with initial state %s", diskLength, string(f))
+	data := generateString(string(f), diskLength)
+	logger.Printf("Calculating checksum of %s", data)
+	checksum := checksum(data)
+	logger.Print(checksum)
+
+	answer := checksum
 	return answer
 }
 
-func SolvePart2(inputFile string, logger *log.Logger) int {
-	f, err := os.Open(inputFile)
+// Optimised to use a string builder
+func generateString2(initialState string, length int) string {
+	strBuilder := strings.Builder{}
+	strBuilder.Grow(length)
+	strBuilder.WriteString(initialState)
+	for len(strBuilder.String()) < length {
+		a := strBuilder.String()
+		strBuilder.WriteRune('0')
+		for i := len(a) - 1; i >= 0; i-- {
+			char := a[i]
+			switch char {
+			case '1':
+				strBuilder.WriteRune('0')
+			case '0':
+				strBuilder.WriteRune('1')
+			}
+		}
+	}
+	return strBuilder.String()[:length]
+}
+
+// Optimised to use a string builder
+func checksum2(str string) string {
+	strBuilder := strings.Builder{}
+	strBuilder.WriteString(str)
+	for len(strBuilder.String())%2 == 0 {
+		prev := strBuilder.String()
+		strBuilder.Reset()
+		strBuilder.Grow(len(prev) / 2)
+		for i := 0; i < len(prev)-1; i += 2 {
+			if prev[i] == prev[i+1] {
+				strBuilder.WriteRune('1')
+			} else {
+				strBuilder.WriteRune('0')
+			}
+		}
+	}
+	return strBuilder.String()
+}
+
+func SolvePart2(inputFile string, logger *log.Logger) string {
+	f, err := os.ReadFile(inputFile)
 	if err != nil {
-		logger.Fatalf("error opening file: %v\n", err)
+		logger.Fatalf("error reading file: %v\n", err)
 	}
-	defer f.Close()
 
-	answer := 0
-
-	scanner := bufio.NewScanner(f)
-	i := -1
-	for scanner.Scan() {
-		i++
-
-		// line := scanner.Text()
-		// logger.Printf("%d : %s", i, line)
+	var diskLength int
+	if strings.HasPrefix(inputFile, "example") {
+		diskLength = 20
+	} else {
+		diskLength = 35651584
 	}
+
+	logger.Printf("Generating string of length %d with initial state %s", diskLength, string(f))
+	data := generateString2(string(f), diskLength)
+	logger.Printf("Calculating checksum of %s", data)
+	checksum := checksum2(data)
+	logger.Printf("Checksum: %s", checksum)
+
+	answer := checksum
 	return answer
 }
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -22,8 +22,8 @@ var tests = []testCase{
 	{
 		name:          "Example 1",
 		inputFilePath: "example_1.txt",
-		expectedP1:    1,
-		expectedP2:    0,
+		expectedP1:    "01100",
+		expectedP2:    "01100",
 	},
 }
 
